b2df38b7255c8f78da5dcea96e8ac610f629e0c2
akibmir90 <akibmir14@gmail.com>
2023-03-10 23:08:59 +0530

    EDA and Problem Statement

--- setup.py
+++ setup.py
@@ -13,13 +13,14 @@ def get_requirements(file_path:str)->List[str]:
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+            
     return requirements
 
 setup(
     name = 'mlproject',
     version = '0.0.1',
-    author = 'Aqib'
-    author_email= 'akibmir14@gmail.com'
-    packages = find_packages()
+    author = 'Aqib',
+    author_email= 'akibmir14@gmail.com',
+    packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
